2423709965587db1b85568f20352489d970b0270
Nora Maguire <eva@rigel.moe>
2018-12-09 20:45:34 -0800

    Implemented a Tumblr Downloader
    From FileDB
    
    ...
    What's old is new again

--- better_saves.rb
+++ better_saves.rb
@@ -1,6 +1,34 @@
 require 'redd'
 require 'nokogiri'
-URLMATCHES=/(\S+(?:pixiv.net|twitter.com|tumblr.com|patreon.com|deviantart.com)\/\S+)/
+require './tumblr_handler.rb'
+Tumblr.configure do |config|
+	config.consumer_key = ENV['TUMBLR_KEY']
+	config.consumer_secret = ENV['TUMBLR_SECRET']
+	# config.oauth_token = ENV['TUMBLR_OAUTH']
+	# config.oauth_token_secret = KEYS['TUMBLR_OAUTH_SECRET']
+end
+# def pixiv_extract(url) 
+#   puts "\t\tPixiv"
+# end
+# def twitter_extract(url)  
+#   puts "\t\tTwitter"
+# end
+# def tumblr_extract(url)
+#   puts "\t\tTumblr"
+# def patreon_extract(url) 
+#   puts "\t\tPatreon"
+# end
+# def deviantart_extract(url) 
+#   puts "\t\tDeviantArt"
+# end
+potential_urls={
+  /pixiv.net/ => "Pixiv",# method(:pixiv_extract),
+  /twitter.com/ => "Twitter",# method(:twitter_extract),
+  /tumblr.com/ => "Tumblr",# method(:tumblr_extract),
+  /patreon.com/ => "Patreon",# method(:patreon_extract),
+  /deviantart.com/ => "Deviantart",# method(:deviantart_extract),
+}
+urlmatches=/(\S+(?:#{Regexp.union(potential_urls.keys)})\/\S+)/
 session = Redd.it(user_agent: 'ReddBot', client_id: ENV['REDDIT_KEY'], secret: ENV['REDDIT_SECRET'], username:ENV['REDDIT_USER'], password:ENV['REDDIT_PASS'])
 count=0
 session.me.saved(limit: 100).each{|post| # unfortunatly the hard limit is 100 from the api (darn, guess I'll have to get creative then)
@@ -10,17 +38,20 @@ session.me.saved(limit: 100).each{|post| # unfortunatly the hard limit is 100 fr
     next unless (comment.class==Redd::Models::Comment)
     n_post=Nokogiri::HTML(comment.body_html)
     possible_links=n_post.xpath('.//a/@href').map{|links| links.to_s}.join(' ')
-    matches=URLMATCHES.match(possible_links)
+    matches=urlmatches.match(possible_links)
     unless(matches.nil?)
-      puts "\t Comment_Link: #{matches[0]}"
+      puts "\tComment_Link: #{matches[0]}"
+      # Ok we know it matches something ... let's find out what
+      potential_urls.each{|k,v|
+        match=k.match(possible_links)
+        if (match.nil?)
+          next
+        end
+        # and we now know we have an actual match, sweet
+        puts v
+      }
     end
-    # .each{|link|
-    #   unless(URLMATCHES.match(link.to_s).nil?)
-    #     puts "\tComment_Link: #{link.to_s}"
-    #   end
-    # }
   }
-  count+=1
+  count += 1
 }
 puts count
-
---
+++ tumblr_handler.rb
@@ -0,0 +1,61 @@
+require 'tumblr_client'
+require 'fileutils'
+require 'rest-client'
+Tumblr.configure do |config|
+  config.consumer_key = ENV['TUMBLR_KEY']
+  config.consumer_secret = ENV['TUMBLR_SECRET']
+end
+FOLDER_LOCATION='./'
+@tumblr_client=Tumblr::Client.new(client: :httpclient)
+def getTumblrPost_Filter(loc)
+  #ok just for te sake of sanity ... 
+  standard_post=/^(?:http||https):\/\/\S+.tumblr.com\/post\/\d+\/(?:[^\/]+)$/
+  cutdown_post=/((?:http||https):\/\/\S+.tumblr.com\/post\/\d+\/\S+)\/\S+/
+  loc=cutdown_post.match(loc)[1] if(standard_post.match(loc).nil?)
+  getTumblrPost(loc)
+end
+def getTumblrPost(loc)
+  @tumblr_client=Tumblr::Client.new(client: :httpclient)
+  tDomain = getTumblrDomain loc
+  tPost = getTumblrPostID loc
+  i = 0
+  download = []
+  get = @tumblr_client.posts(tDomain, id: tPost.to_i)['posts'][0]['photos']
+  get.each do |img|
+    download[i] = img['original_size']['url']
+    i += 1
+  end
+  fileLoc = ''
+  fileLoc = if get.length > 1
+              "#{FOLDER_LOCATION}/Tumblr/#{tPost}/"
+
+            else
+              "#{FOLDER_LOCATION}/Tumblr/"
+            end
+  FileUtils.mkdir_p fileLoc # covering our ass if the directory does not exist.
+  download.each do |link|
+    getFileFromURL(link, fileLoc)
+  end
+  true
+  # rescue NoMethodError
+  # puts "ERROR: It's likely that this particular link is not a link to a photo post: #{loc}"
+end
+def getTumblrDomain(loc)
+  tURL = loc.split('/')
+  tDomain = tURL[2]
+end
+
+def getTumblrPostID(loc)
+  tURL = loc.split('/')
+  tPost = tURL[-1]
+  tPost = tURL[-2] unless /^[\d]+(\.[\d]+){0,1}$/ === tPost # We are checking to see if we got the actual tumblr post id.
+  tPost
+end
+def getFileFromURL(webLoc,folderLoc)
+  if(folderLoc[-1]!="/")
+    folderLoc=folderLoc+"/"
+  end
+  open(folderLoc+webLoc.split("/")[-1], 'wb') do |file|
+    file << RestClient::get(webLoc)
+  end
+end
